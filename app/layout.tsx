4bb59c23eec43a5af199b09d1811a75c78c539ae
David Hurley <dhurls99@gmail.com>
2024-06-15 09:11:56 -0400

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
-
+import { Analytics } from '@/components/analytics';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import Privy from '@/components/privy';
@@ -28,6 +28,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
+        <Analytics />
         <Privy>{children}</Privy>
       </body>
     </html>
