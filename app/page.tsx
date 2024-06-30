5853ac5344b6282aac80fca7d08791aee8644538
David Hurley <dhurls99@gmail.com>
2024-06-30 14:32:01 -0400

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ import CollectPage from '@/components/collect/collect-page';
 
 export default function Home() {
   return (
-    <div>
-      ok
-      <CollectPage collectionAddress="0xE79D517C958233A1339E8C1695fC87fD5013d7D1" />
-    </div>
+    <CollectPage collectionAddress="0xE79D517C958233A1339E8C1695fC87fD5013d7D1" />
   );
 }
