3d2acd3641c1aa18dc337aba7926cda303e60ee2
David Hurley <dhurls99@gmail.com>
2024-07-04 22:53:53 -0400

    set new heompage collection

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import CollectPage from '@/components/collect/collect-page';
 
 export default function Home() {
   return (
-    <CollectPage collectionAddress="0xE79D517C958233A1339E8C1695fC87fD5013d7D1" />
+    <CollectPage
+      collectionAddress={process.env.HOMEPAGE_COLLECTION as `0x${string}`}
+    />
   );
 }
